a24bc1f05c00d796978b08bb06fb555e045deaa3
anil <993421281@qq.com>
2019-04-04 19:59:13 +0800

    detail _list

---
+++ pages/detail/_id.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="text-center">
+        <br><br>
+        <br><br>
+        {{ id }}
+        <br><br>
+        <br><br>
+    </div>
+</template>
+
+<script>
+export default {
+    validate ({ params }) {
+        // 必须是number类型
+        return /^\d+$/.test(params.id)
+    },
+    computed: {
+        id () {
+            return this.$router.history.current.params.id
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
